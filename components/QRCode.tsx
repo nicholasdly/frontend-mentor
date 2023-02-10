19df420760e095341cd0ac40780ec04e99e2b3a7
Nicholas Ly <nichdly@gmail.com>
2023-02-10 12:41:33 -0500

    Minor changes to improve submission accuracy

--- components/QRCode.tsx
+++ components/QRCode.tsx
@@ -1,14 +1,14 @@
 
 export default function QRCode() {
     return (
-        <div class="bg-white w-[375px] rounded-3xl text-center">
+        <div class="bg-white w-[330px] rounded-3xl text-center">
             <div class="m-5 rounded-2xl overflow-hidden">
-                <image src='image-qr-code.png' />
+                <image src='image-qr-code.png' alt='QR Code' />
             </div>
-            <p class="text-primary font-bold text-2xl px-12 pt-2">
+            <p class="text-primary font-bold text-2xl px-10 pt-2">
                 Improve your front-end skills by building projects
             </p>
-            <p class="text-accent text-lg px-12 pt-5 pb-12">
+            <p class="text-accent text-lg px-8 pt-5 pb-10">
                 Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
             </p>
         </div>
--- routes/index.tsx
+++ routes/index.tsx
@@ -7,8 +7,6 @@ export default function Home() {
     <body class="font-outfit bg-background">
 
       <Head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
         <title>Frontend Mentor | QR code component</title>
       </Head>
